b093ddc4f90853de340c7885c54880e4a105a93e
Cristhian Duran <durancristhian@gmail.com>
2016-02-03 16:16:22 -0300

    Add follow number UI

--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,24 @@
 import $ from "jquery";
 import { getResults } from "./api";
 
-getResults(results => {
-    $("#loading").addClass("hide");
-    $("#nacional-primera").html(results.nacional.laPrimera);
-    $("#nacional-matutina").html(results.nacional.matutina);
-    $("#nacional-vespertina").html(results.nacional.vespertina);
-    $("#nacional-nocturna").html(results.nacional.nocturna);
-    $("#provincia-matutina").html(results.provincia.matutina);
-    $("#provincia-vespertina").html(results.provincia.vespertina);
-    $("#provincia-nocturna").html(results.provincia.nocturna);
-    $("#provincia-primera").html(results.provincia.laPrimera);
-    $("#table").removeClass("hide");
-});
+if (window.location.pathname === "/") {
+    getResults(results => {
+        $("#loading").addClass("hide");
+        $("#nacional-primera").html(results.nacional.laPrimera);
+        $("#nacional-matutina").html(results.nacional.matutina);
+        $("#nacional-vespertina").html(results.nacional.vespertina);
+        $("#nacional-nocturna").html(results.nacional.nocturna);
+        $("#provincia-matutina").html(results.provincia.matutina);
+        $("#provincia-vespertina").html(results.provincia.vespertina);
+        $("#provincia-nocturna").html(results.provincia.nocturna);
+        $("#provincia-primera").html(results.provincia.laPrimera);
+        $("#table").removeClass("hide");
+    });
+} else {
+    $(".js-toggle").on("click", (event) => {
+
+        event.preventDefault();
+
+        $(event.target).toggleClass("toggle-active");
+    });
+}
