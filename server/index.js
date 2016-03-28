897d9c07faae59490a1b569d4ee3114ec6809f91
Cristhian Duran <durancristhian@gmail.com>
2016-03-28 01:05:00 -0300

    Add error handler in express

--- server/index.js
+++ server/index.js
@@ -71,6 +71,10 @@ app.use("/app", appController);
 app.use("/auth/twitter", twitterController);
 app.use("/logout", logoutController);
 
+/* eslint-disable no-unused-vars */
+app.use((error, req, res, next) => res.send(500, error));
+/* eslint-enable no-unused-vars */
+
 mongoose.connect(process.env.DB);
 
 mongoose.connection.on("error", (error) => {
